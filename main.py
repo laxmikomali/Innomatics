ff5a775594b5483257a8b2b40a5bd0d3e9fe2e23
laxmikomali <147476147+laxmikomali@users.noreply.github.com>
2026-08-11 09:29:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI(title="Komali API")
+app = FastAPI(title="Student Details Management API")
+students_db = {
+    1: {"name": "Aarav", "age": 21, "course": "Data Science"},
+    2: {"name": "Priya", "age": 22, "course": "Web Development"},
+    3: {"name": "Rohan", "age": 20, "course": "AI & ML"},
+}
+class Student(BaseModel):
+    name: str
+    age: int
+    course: str
+@app.get("/students/")
+def get_students(course: str = None):
+    if course:
+        filtered = {
+            s_id: s
+            for s_id, s in students_db.items()
+            if s["course"].lower() == course.lower()
+        }
+        return filtered
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/name")
-def read_name():
-    return {"name": "Komali"}
-
-@app.get("/batch")
-def read_batch():
-    return {"batch": "Batch 55B"}
+    return students_db
